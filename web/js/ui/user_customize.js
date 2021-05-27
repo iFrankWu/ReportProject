a9b0839b75261f5d732ef933df607ae2abd44b9e
FrankWu <wushexin2006@126.com>
2021-05-27 22:10:49 +0800

    修正方法

--- web/js/ui/user_customize.js
+++ web/js/ui/user_customize.js
@@ -329,4 +329,14 @@ function readURL(input) {
     }
 };
 
+String.prototype.startWith=function(str){
+    var reg=new RegExp("^"+str);
+    return reg.test(this);
+}
+
+String.prototype.endWith=function(str){
+    var reg=new RegExp(str+"$");
+    return reg.test(this);
+}
+
 
